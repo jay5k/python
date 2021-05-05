b786da4ad5ab08bd8e582ad118b0ec3860ffbda7
jay5k <gregory.jermaine@gmail.com>
2021-05-04 22:23:50 -0600

    Messing around

--- April/ex39 Dictionaries.py
+++ April/ex39 Dictionaries.py
@@ -1,24 +1,24 @@
-things = ['a', 'b', 'c', 'd']
-print(things[1])
-things[1] = 'z'
-print(things[1])
+#things = ['a', 'b', 'c', 'd']
+#print(things[1])
+#things[1] = 'z'
+#print(things[1])
 
-stuff = {'name': 'Zed', 'age': 39, 'height': 6* 12 + 2}
-print(stuff['name'])
-print(stuff['age'])
-print(stuff['height'])
-stuff['city'] = "SF"
-print(stuff['city'])
+#stuff = {'name': 'Zed', 'age': 39, 'height': 6* 12 + 2}
+#print(stuff['name'])
+#print(stuff['age'])
+#print(stuff['height'])
+#stuff['city'] = "SF"
+#print(stuff['city'])
 
-stuff[1] = 'Wow'
-stuff[2] = 'Neato'
-print(stuff[1])
-print(stuff[2])
+#stuff[1] = 'Wow'
+#stuff[2] = 'Neato'
+#print(stuff[1])
+#print(stuff[2])
 
-del stuff['city']
-del stuff[1]
-del stuff[2]
-print(stuff)
+#del stuff['city']
+#del stuff[1]
+#del stuff[2]
+#print(stuff)
 
 #########################################################################
 
@@ -28,7 +28,7 @@ states = {
     'Florida': 'FL',
     'California': 'CA',
     'New York': 'NY',
-    'Micigan': 'MI'
+    'Michigan': 'MI'
 }
 
 #create a basic set of states and some cities in them
@@ -47,3 +47,39 @@ print('-' * 10)
 print('NY State has: ', cities['NY'])
 print('OR State has: ', cities['OR'])
 
+#print some states
+print('-' * 10 )
+print("Michigan's abbreviation is: ", states['Michigan'])
+print("Florida's abbreviation is ", states['Florida'])
+
+#do it by using the state then cities dictionary
+print('-' * 10)
+print("Michigan has: ", cities[states['Michigan']])
+print("Florida has: ", cities[states['Florida']])
+
+#print every state abbreviation
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} is abbreviated {abbrev}")
+
+#print every city in state
+print('-' * 10)
+for abbrev, city in list(cities.items()):
+    print(f"{abbrev} has the city {city}")
+
+#now do both at the same time
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} state is abbreviated {abbrev}")
+    print(f"and has city {cities[abbrev]}")
+
+print('-' * 10)
+#safely get a abbreviation by state that might not be there
+state = states.get('Texas')
+
+if not state:
+    print("Sorry, no Texas.")
+
+#get a city with a default value
+city = cities.get('TX', 'Does Not Exist')
+print(f"The city for the state 'TX' is: {city}")
