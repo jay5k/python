2239af4b6d47f83b043c06307e758c0df75f0409
jay5k <gregory.jermaine@gmail.com>
2021-05-18 23:02:50 -0600

    Messing around

--- April/objects.py
+++ April/objects.py
@@ -14,7 +14,9 @@ class Employee:
         Employee.empCount += 1
 
     def DisplayEmpCount(self):
-        print("Total Employees are", empCount)
+        print("Total Employees are", Employee.empCount)
+
+print(Employee.empCount)
 
 emp1 = Employee("Bob",29)
 emp2 = Employee("Reagan",1)
@@ -23,6 +25,28 @@ emp2 = Employee("Reagan",1)
 print(emp1.name)
 
 print(emp1.DisplayEmpCount())
+
+###############################################################################################
+
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+happy_bday = Song(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around tha family",
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
 
 
 
